7797878f416e1211f0347620678d8a57c81fa64c
masterix09 <44431274+masterix09@users.noreply.github.com>
2020-02-05 14:19:10 +0100

    implement the view of team ranking and players ranking

--- Prova/src/Classifica.java
+++ Prova/src/Classifica.java
@@ -6,6 +6,8 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.JScrollPane;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
@@ -23,16 +25,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import javax.swing.JLabel;
 import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class Classifica extends JDialog {
 	private JTable table;
+	private JButton btnChange;
+	private JButton CHANGE;
 	
 
 	public Classifica(Driver drive){
 		setTitle("CLASSIFICA");
 		
 		
-		setBounds(100, 100, 915, 378);
+		setBounds(100, 100, 1058, 372);
 		getContentPane().setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -44,14 +49,72 @@ public class Classifica extends JDialog {
 		scrollPane2.setBounds(69, 45, 571, 230);
 		
 		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setVisible(false);
+		lblNome.setBounds(660, 216, 69, 20);
+		getContentPane().add(lblNome);
+		
+		JLabel lblCognome = new JLabel("Cognome:");
+		lblCognome.setVisible(false);
+		lblCognome.setBounds(660, 169, 94, 20);
+		getContentPane().add(lblCognome);
+		
+		JLabel lblGoalFatti = new JLabel("Goal Fatti:");
+		lblGoalFatti.setVisible(false);
+		lblGoalFatti.setBounds(660, 252, 81, 20);
+		getContentPane().add(lblGoalFatti);
+		
+		JLabel label3 = new JLabel("");
+		label3.setVisible(true);
+		label3.setBounds(764, 252, 124, 20);
+		getContentPane().add(label3);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(754, 216, 124, 20);
+		getContentPane().add(label);
+		
+		JLabel label2 = new JLabel("");
+		label2.setBounds(771, 169, 124, 20);
+		getContentPane().add(label2);
+		
+		
+		
+		
+		
 		CreaTabella(scrollPane);
 		
 		drive.PopolaTabellaClassifica(table);
 		
 		
-		JButton btnChange = new JButton("CLASSIFICA SQUADRA");
+		btnChange = new JButton("CLASSIFICA SQUADRA");
 		btnChange.setVisible(false);
-		JButton CHANGE = new JButton("CLASSIFICA MARCATORI");
+		btnChange.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				scrollPane2.setVisible(false);
+				btnChange.setVisible(false);
+				CHANGE.setVisible(true);
+				scrollPane.setVisible(true);
+				getContentPane().add(scrollPane);
+				CreaTabella(scrollPane);
+				drive.PopolaTabellaClassifica(table);
+				scrollPane.setViewportView(table);
+				lblNome.setVisible(false);
+				lblCognome.setVisible(false);
+				lblGoalFatti.setVisible(false);
+				label.setVisible(false);
+				label2.setVisible(false);
+				label3.setVisible(false);
+				
+				
+			}
+		});
+		btnChange.setBounds(653, 63, 223, 29);
+		getContentPane().add(btnChange);
+		
+		
+		
+		CHANGE = new JButton("CLASSIFICA MARCATORI");
 		CHANGE.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
@@ -68,31 +131,42 @@ public class Classifica extends JDialog {
 				drive.PopolaTabellaMarcatori(table);
 				scrollPane2.setViewportView(table);
 				
+
+				table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+					
+					
+					@Override
+					public void valueChanged(ListSelectionEvent e) {
+						
+						
+						lblNome.setVisible(true);
+						lblCognome.setVisible(true);
+						lblGoalFatti.setVisible(true);
+						label.setVisible(true);
+						label2.setVisible(true);
+						label3.setVisible(true);
+						label.setText(table.getValueAt(table.getSelectedRow(), 0).toString());
+						label2.setText(table.getValueAt(table.getSelectedRow(), 1).toString());
+						label3.setText(table.getValueAt(table.getSelectedRow(), 2).toString());
+						
+					}
+				});
 				
 					
 				
 			}
 		});
-		CHANGE.setBounds(653, 63, 201, 29);
+		CHANGE.setBounds(653, 63, 225, 29);
 		getContentPane().add(CHANGE);
 		
 		
-		btnChange.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				scrollPane2.setVisible(false);
-				btnChange.setVisible(false);
-				CHANGE.setVisible(true);
-				scrollPane.setVisible(true);
-				getContentPane().add(scrollPane);
-				CreaTabella(scrollPane);
-				drive.PopolaTabellaClassifica(table);
-				scrollPane.setViewportView(table);
-				
-			}
-		});
-		btnChange.setBounds(655, 163, 223, 29);
-		getContentPane().add(btnChange);
+		
+		
+		
+
+		
+		
+		
 		
 		
 		
@@ -133,7 +207,6 @@ public class Classifica extends JDialog {
 		scrollPane2.setViewportView(table);
 		
 	}
-	
 }
 	
 	
